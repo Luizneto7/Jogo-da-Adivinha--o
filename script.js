da2ab6101c3f29481003ceeab2afccf2471a049b
luizneto7 <luiz.bnp2@gmail.com>
2023-12-22 18:58:10 -0300

    code refactoring

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const screen1 = document.querySelector('.screen1')
 const screen2 = document.querySelector('.screen2')
-
-const randomNumber = Math.round(Math.random() * 10)
+const btnTry = document.querySelector("#btnTry")
+const btnTryAgain = document.querySelector("#btnTryAgain")
+let randomNumber = Math.round(Math.random() * 10)
 let xAttepmpts = 1
+
+btnTry.addEventListener('click', handleTryClick)
+btnTryAgain.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', handleResetEnter)
 
 function handleTryClick(event) {
     event.preventDefault()
@@ -18,18 +23,25 @@ function handleTryClick(event) {
     
     if(xAttepmpts == 1){
         document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttepmpts} tentativa!`
-    }
-
+    } 
+    
     inputNumber.value = ""
     xAttepmpts++
 }
 
-const btnTry = document.querySelector("#btnTry")
-const btnTryAgain = document.querySelector("#btnTryAgain")
+function handleResetClick(){
+    toggleScreen();
+    xAttepmpts = 1;
+    randomNumber = Math.round(Math.random() * 10);
+}
 
-btnTry.addEventListener('click', handleTryClick)
-btnTryAgain.addEventListener('click', function(){
-    screen2.classList.add("hide")
-    screen1.classList.remove("hide")
-    xAttepmpts = 1
-})
+function toggleScreen(){
+    screen2.classList.toggle("hide")
+    screen1.classList.toggle("hide")
+}
+
+function handleResetEnter(e){
+    if(e.key = 'Enter' && screen1.classList.contains("hide")){
+        handleResetClick();
+    }
+}
